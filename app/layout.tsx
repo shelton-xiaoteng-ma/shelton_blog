166cb64cbe0d362a636b6877e94a906cfe56d712
maxiaoteng <jizhuwo00@hotmail.com>
2024-12-16 17:44:35 +0800

    Verify site ownership

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,6 +91,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         href={`${basePath}/static/favicons/safari-pinned-tab.svg`}
         color="#5bbad5"
       />
+      <meta name="google-adsense-account" content="ca-pub-5335003214354505"></meta>
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
