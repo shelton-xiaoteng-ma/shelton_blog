d7959fed400c896ceec3423796bac077c463fa1c
maxiaoteng <jizhuwo00@hotmail.com>
2024-12-28 16:13:09 +0800

    embem youtube

--- components/Card.tsx
+++ components/Card.tsx
@@ -16,7 +16,7 @@ const Card = ({ title, description, imgSrc, href, videoSrc }) => (
             src={videoSrc}
             title={title}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            // referrerPolicy="strict-origin-when-cross-origin"
+            referrerPolicy="strict-origin-when-cross-origin"
             allowFullScreen
             className="h-full w-full"
           ></iframe>
@@ -29,20 +29,16 @@ const Card = ({ title, description, imgSrc, href, videoSrc }) => (
         )}
       </div>
       <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              {title}
-            </Link>
-          ) : (
-            title
-          )}
+        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight hover:text-primary-500 hover:underline">
+          <Link href={href ? href : '#'} aria-label={`Link to ${title}`}>
+            {title}
+          </Link>
         </h2>
         <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
         {href && (
           <Link
             href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 hover:underline dark:hover:text-primary-400"
             aria-label={`Link to ${title}`}
           >
             Learn more &rarr;
