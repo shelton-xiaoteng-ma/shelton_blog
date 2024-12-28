263228a56be165189ed03808e00e46d7046eb3aa
maxiaoteng <jizhuwo00@hotmail.com>
2024-12-28 16:41:07 +0800

    fix: update og:image handling and change images type to string array
    - Fixed incorrect `og:image` meta tag setup
    - Updated `images` type from `any` to `string[]` for improved type safety

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -103,7 +103,7 @@ export const Blog = defineDocumentType(() => ({
     lastmod: { type: 'date' },
     draft: { type: 'boolean' },
     summary: { type: 'string' },
-    images: { type: 'json' },
+    images: { type: 'list', of: { type: 'string' }, default: [] },
     authors: { type: 'list', of: { type: 'string' } },
     layout: { type: 'string' },
     bibliography: { type: 'string' },
